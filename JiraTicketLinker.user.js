00c56d7f2db4aad404c8036f6d0829a52bfdaf0c
prawnpie <github@dirtmine.com>
2018-02-17 09:09:51 -0800

    organize into functions

--- JiraTicketLinker.user.js
+++ JiraTicketLinker.user.js
@@ -9,42 +9,71 @@
 // @require http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 (function() {
-
+    'use strict';
 
     // TODO:
     // * refactor shared code for adding secondLine links
     // * add link when viewing commit details page
 
-
-    'use strict';
-
     var jiraTicketPath = "https://jira.your_org.com/browse/";
     var linkTemplate = '<span class="d-inline-block">[<a class="muted-link" href="' + jiraTicketPath + 'ISSUE_ID">ISSUE_ID</a>]</span>&nbsp;';
 
     function doIt() {
-
         var path = window.location.pathname;
-
-        // PR list - /org/proj/pulls
         if (path.match(/^\/.*\/.*\/pull\/\d+$/)) {
-            console.log("doing single pull request details");
-            // insert link into titles on PR page
-            $("span.js-issue-title").html(function() {
-                return $(this).html().replace(/\[([A-Z]+-\d+)\]/g, '[<a href="' + jiraTicketPath + '$1">$1</a>]');
-            });
+            // PR details: /org/proj/pull/12345
+            handlePrDetail();
+        } else if (path.match(/^\/.*\/.*\/pulls$/)) {
+            // PR list - /org/proj/pulls
+            handlePrList();
+        } else if (path.match(/^\/.*\/.*\/commits\/.*/) || path.match(/^\/.*\/.*\/pull\/\d+\/commits$/)) {
+            // pr commits: /org/proj/pull/####/commits
+            // branch commits: /org/proj/commits/[branchName]
+
+            handleCommitList();
         }
+    }
 
-        // PR details: /org/proj/pull/12345
-        else if (path.match(/^\/.*\/.*\/pulls$/)) {
-            console.log("doing pull request list");
-            // add links into row on the PR list page
-            var issueDivs = $("ul.js-active-navigation-container li div div:nth-of-type(3)");
+    function handlePrDetail() {
+        console.log("doing single pull request details");
+        // insert link into titles on PR page
+        $("span.js-issue-title").html(function() {
+            return $(this).html().replace(/\[([A-Z]+-\d+)\]/g, '[<a href="' + jiraTicketPath + '$1">$1</a>]');
+        });
+    }
 
-            for (var i = 0; i < issueDivs.length; i++) {
-                var issueDiv = $(issueDivs[i]);
-                var link = $("a.js-navigation-open", issueDiv)[0];
-                var linkText = $(link).html();
-                var secondLine = $("div.mt-1", issueDiv)[0];
+    function handlePrList() {
+        console.log("doing pull request list");
+        // add links into row on the PR list page
+        var issueDivs = $("ul.js-active-navigation-container li div div:nth-of-type(3)");
+
+        for (var i = 0; i < issueDivs.length; i++) {
+            var issueDiv = $(issueDivs[i]);
+            var link = $("a.js-navigation-open", issueDiv)[0];
+            var linkText = $(link).html();
+            var secondLine = $("div.mt-1", issueDiv)[0];
+            var matches = linkText.match(/\[[A-Z]+-\d+\]/g);
+            if (matches) {
+                for (var j = 0; j < matches.length; j++) {
+                    var match = matches[j];
+                    var issueId = /\[([A-Z]+-\d+)\]/g.exec(match)[1];
+                    var linkHtml = linkTemplate.replace(/ISSUE_ID/g, issueId);
+                    $(secondLine).append(linkHtml);
+                }
+            }
+        }
+    }
+
+    function handleCommitList() {
+        console.log("doing commits");
+        var commitTitleParagraphs = $("p.commit-title");
+        if (commitTitleParagraphs) {
+            for (var i = 0; i < commitTitleParagraphs.length; i++) {
+                var commitTitleParagraph = commitTitleParagraphs[i];
+                var commitCell = commitTitleParagraph.parentElement;
+                var commitLink = $("a", commitTitleParagraph)[0];
+                var linkText = $(commitLink).html();
+                var secondLine = $("div.commit-meta", commitCell)[0];
                 var matches = linkText.match(/\[[A-Z]+-\d+\]/g);
                 if (matches) {
                     for (var j = 0; j < matches.length; j++) {
@@ -53,32 +82,6 @@
                         var linkHtml = linkTemplate.replace(/ISSUE_ID/g, issueId);
                         $(secondLine).append(linkHtml);
                     }
-                }
-            }
-        }
-
-        // pr commits: /org/proj/pull/####/commits
-        // branch commits: /org/proj/commits/[branchName]
-        else if (path.match(/^\/.*\/.*\/commits\/.*/) || path.match(/^\/.*\/.*\/pull\/\d+\/commits$/)) {
-            console.log("doing commits");
-            var commitTitleParagraphs = $("p.commit-title");
-            if (commitTitleParagraphs) {
-                for (var i = 0; i < commitTitleParagraphs.length; i++) {
-                    var commitTitleParagraph = commitTitleParagraphs[i];
-                    var commitCell = commitTitleParagraph.parentElement;
-                    var commitLink = $("a", commitTitleParagraph)[0];
-                    var linkText = $(commitLink).html();
-                    var secondLine = $("div.commit-meta", commitCell)[0];
-                    var matches = linkText.match(/\[[A-Z]+-\d+\]/g);
-                    if (matches) {
-                        for (var j = 0; j < matches.length; j++) {
-                            var match = matches[j];
-                            var issueId = /\[([A-Z]+-\d+)\]/g.exec(match)[1];
-                            var linkHtml = linkTemplate.replace(/ISSUE_ID/g, issueId);
-                            $(secondLine).append(linkHtml);
-                        }
-                    }
-
                 }
             }
         }
